b82dc5f75b96c72b8b18c4495daf7e5c1c44d3ad
carttman <chj990920@gmail.com>
2025-05-21 18:23:31 +0900

    Update PlayerAnimation.cs

--- Assets/Character/Player/PlayerAnimation.cs
+++ Assets/Character/Player/PlayerAnimation.cs
@@ -7,18 +7,15 @@ public class PlayerAnimation : NetworkBehaviour
     [SerializeField] private Animator animator;
     private NewPlayerMovement movement;
     
-     //private float MoveSpeed;
     private void Awake()
     {
        
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         movement = GetComponent<NewPlayerMovement>();
     }
 
-    // Update is called once per frame
     void Update()
     {
     }
