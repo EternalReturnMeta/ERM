e4ed9e8eb0b77c0aa43fb0373148389de1f04820
carttman <chj990920@gmail.com>
2025-05-29 13:48:43 +0900

    unusable code

--- Assets/Character/Player/HeroMovement.cs
+++ Assets/Character/Player/HeroMovement.cs
@@ -7,7 +7,6 @@ using UnityEngine.AI;
 
 public class HeroMovement : NetworkBehaviour
 {
-    private Camera mainCamera;
     [SerializeField] private LayerMask groundLayer; // 바닥 레이어
     [HideInInspector] public NavMeshAgent navMeshAgent;
     private SimpleKCC kcc;
@@ -15,8 +14,7 @@ public class HeroMovement : NetworkBehaviour
     [Networked] public NetworkButtons ButtonsPrevious { get; set; }
 
     private HeroInput heroInput;
-    private int moveCheck;
-    public event Action<int> OnMove;
+    public event Action<int> OnMoveVelocityChanged;
 
     public float baseSpeed;
     private NavMeshPath path;
@@ -26,16 +24,10 @@ public class HeroMovement : NetworkBehaviour
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         path = new NavMeshPath();
-        moveCheck = 0;
 
         kcc = GetComponentInChildren<SimpleKCC>();
     }
     
-    private void Start()
-    {
-        mainCamera = Camera.main;
-    }
-
     public override void Spawned()
     {   
         // 클라이언트 agent 비활성화
@@ -43,29 +35,20 @@ public class HeroMovement : NetworkBehaviour
         {
             navMeshAgent.enabled = false;
         }
-        // 호스트/서버 agent 업데이트 비활성화
-        else
-        {
-            //navMeshAgent.updatePosition = false;
-        }
     }
 
     public override void FixedUpdateNetwork() 
     {
         if (!HasStateAuthority)
             return;
-        
+
+        PathCalculateAndMove();
+    }
+
+    private void PathCalculateAndMove()
+    {
         if (GetInput(out heroInput))
         {
-            // if(heroInput.Buttons.WasPressed(ButtonsPrevious, InputButton.RightClick))
-            // {
-            //     ToggleProcess(true);
-            // }
-            // if (heroInput.Buttons.WasReleased(ButtonsPrevious, InputButton.RightClick))
-            // {
-            //     ToggleProcess(false);
-            // }
-
             if (heroInput.HitPosition_RightClick != Vector3.zero)
             {
                 lastPos = heroInput.HitPosition_RightClick;
@@ -73,7 +56,14 @@ public class HeroMovement : NetworkBehaviour
         }
         ButtonsPrevious = heroInput.Buttons;
         
+        // 길찾기 경로 계산
         navMeshAgent.CalculatePath(lastPos, path);
+        
+        if (path.corners.Length <= 0)
+        {   //정지 애니메이션 
+            OnMoveVelocityChanged?.Invoke(0);
+        }
+        
         if (path.corners != null && path.corners.Length > 0)
         {
             Vector3 nextWaypoint;//
@@ -85,69 +75,25 @@ public class HeroMovement : NetworkBehaviour
             {
                 nextWaypoint = path.corners[1];
             }
+            
             var dist = Vector3.Distance(kcc.Position, nextWaypoint);
             
             if (dist <= navMeshAgent.stoppingDistance && path.corners.Length <= 2)
             {
-                 OnMove?.Invoke(0);
-                 return;
+                OnMoveVelocityChanged?.Invoke(0);
+                return;
             }
-            
-            OnMove?.Invoke(1);
             
             var speed = baseSpeed * (60f / Runner.TickRate);
             var direction = (nextWaypoint - kcc.Position).normalized;
             kcc.Move(direction * speed);
+            OnMoveVelocityChanged?.Invoke(1);
             
             float targetYaw = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float currentPitch = kcc.GetLookRotation(true, false).x;
+            
             kcc.SetLookRotation(currentPitch, targetYaw);
             
         }
     }
-
-    public override void Render()
-    {
-        if (HasStateAuthority)
-        {
-            if (navMeshAgent)
-            {
-                if (navMeshAgent.remainingDistance < 0.1f)
-                {
-                    if (moveCheck == 1)
-                    {
-                        //OnMove?.Invoke(0);
-                        moveCheck = 0;
-                    }
-                }
-            }
-        }
-    }
-    
-    // private void ToggleProcess(bool isOn)
-    // {
-    //     if (isOn)
-    //     {
-    //         StartCoroutine(SetPositionProcess());
-    //     }
-    //     else
-    //     {
-    //         StopAllCoroutines();
-    //         moveCheck = 1;
-    //     }
-    // }
-    //
-    // private IEnumerator SetPositionProcess()
-    // {
-    //     while (true)
-    //     {
-    //         OnMove?.Invoke(1);
-    //         
-    //         navMeshAgent.isStopped = false;
-    //         //navMeshAgent.SetDestination(heroInput.HitPosition);
-    //         
-    //         yield return new WaitForSeconds(0.15f);
-    //     }
-    // }
-    
 }
